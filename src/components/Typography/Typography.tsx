ec76eab19f13425635c51f82a72da0fcec2a1b37
Federico Giorgino <federicog.dev@gmail.com>
2023-01-09 20:56:22 +0100

    Typography

---
+++ src/components/Typography/Typography.tsx
@@ -0,0 +1,59 @@
+import { useTheme } from "@react-navigation/native";
+import React, { FC } from "react";
+import { Text, TextProps, TextStyle } from "react-native";
+import { styles } from "./styles";
+
+interface ITypographyProps extends TextProps {
+  style?: TextStyle | TextStyle[];
+  variant?: "light" | "regular" | "bold" | "mid";
+  color?: "primary" | "text" | "subtext";
+  size?: number;
+}
+
+const Typography: FC<ITypographyProps> = ({
+  children,
+  variant,
+  style,
+  color = "text",
+  size = 16,
+  ...rest
+}) => {
+  const { colors } = useTheme();
+  let textStyle;
+  switch (variant) {
+    case "light":
+      textStyle = styles.light;
+      break;
+    case "regular":
+      textStyle = styles.regular;
+      break;
+    case "bold":
+      textStyle = styles.bold;
+      break;
+    case "mid":
+      textStyle = styles.mid;
+      break;
+    default:
+      textStyle = styles.regular;
+      break;
+  }
+
+  const passedStyles = Array.isArray(style)
+    ? Object.assign({}, ...style)
+    : style;
+
+  return (
+    <Text
+      style={[
+        textStyle,
+        { color: colors[color], fontSize: size },
+        { ...passedStyles },
+      ]}
+      {...rest}
+    >
+      {children}
+    </Text>
+  );
+};
+
+export default Typography;
---
+++ src/components/Typography/styles.ts
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  light: { fontWeight: "300" },
+  regular: { fontWeight: "normal" },
+  mid: { fontWeight: "600" },
+  bold: { fontWeight: "700" },
+});
